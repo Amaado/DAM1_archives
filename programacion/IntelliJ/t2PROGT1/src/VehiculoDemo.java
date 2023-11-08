3c2077dac6529a7b0fb496cc32032f30e0a1ba83
Amaado <andresamadocibreiro22@gmail.com>
2023-11-08 21:53:58 +0100

    08/11/23

--- programacion/IntelliJ/t2PROGT1/src/VehiculoDemo.java
+++ programacion/IntelliJ/t2PROGT1/src/VehiculoDemo.java
@@ -1,8 +1,10 @@
-public class VehiculoDemo {
+class VehiculoDemo {
     public static void main(String[] args) {
 // Creamos dos nuevas instancias de la clase Vehiculo
         Vehiculo miCoche = new Vehiculo();
         Vehiculo furgoPepe = new Vehiculo();
+
+        final int DIST_SCQ_MAD=600;
 // asignamos valores a los atributos de miCoche
         miCoche.numPasajeros = 5;
         miCoche.capDeposito = 45;
@@ -11,11 +13,10 @@ public class VehiculoDemo {
         furgoPepe.numPasajeros = 7;
         furgoPepe.capDeposito = 85;
         furgoPepe.consumo100KM = 7.25;
-// calculamos la distancia máxima que puede recorrer
-        double dist1 = miCoche.capDeposito * 100.0 / miCoche.consumo100KM;
-        double dist2 = furgoPepe.capDeposito * 100.0 / furgoPepe.consumo100KM;
-        System.out.printf("miCoche puede llevar %d ", miCoche.numPasajeros);
-        System.out.printf("personas hasta %.2f kms%n", dist1);
-        System.out.printf("furgoPepe puede llevar %d ", furgoPepe.numPasajeros);
-        System.out.printf("personas hasta %.2f kms%n", dist2);
+
+        System.out.print("miCoche puede llevar " + miCoche.numPasajeros + " per. ");
+        miCoche.autonomia();    
+        System.out.print("furgoPepe puede llevar " + furgoPepe.numPasajeros + " per. ");
+        miCoche.autonomia();
+    }
 }
