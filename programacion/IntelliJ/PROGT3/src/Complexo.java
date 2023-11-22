2abf992fcfe01e5c1d8df7a2b1a8b10bb96f83f8
Amaado <andresamadocibreiro22@gmail.com>
2023-11-22 21:51:55 +0100

    22/11/23_2

---
+++ programacion/IntelliJ/PROGT3/src/Complexo.java
@@ -0,0 +1,36 @@
+public class Complexo {
+    private double real;
+    private double imax;
+
+
+    public Complexo (){
+        real=0;
+        imax=0;
+    }
+    public Complexo (double real, double imax){
+        this.real=real;
+        this.imax=imax;
+    }
+    public double getconsultareal (){
+        return real;
+    }
+    public double getconsultaimax (){
+        return imax;
+    }
+    void setcambiareal (double real){
+        this.real=real;
+    }
+
+    void setcambiaimax (double imax){
+        this.imax=imax;
+    }
+    
+    @Override
+    public String toString() {
+        return real+imax+"i";
+    }
+    public void Sumar(Complexo b){
+        this.real=this.real+b.real;
+        this.imax=this.imax+b.imax;
+    }
+}
