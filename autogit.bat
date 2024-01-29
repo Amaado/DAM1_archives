0b09c387f603876f122bfdab1127306bfb77d5fe
Amaado <andresamadocibreiro22@gmail.com>
2024-01-29 17:02:29 +0100

    autoCommit 2024/01/29. Hora: 17:02

---
+++ autogit.bat
@@ -0,0 +1,6 @@
+cd C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives
+git add --all
+git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
+git push
+autogit_msg.txt
+exit
