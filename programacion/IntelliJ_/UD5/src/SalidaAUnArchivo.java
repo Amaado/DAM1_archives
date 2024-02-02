92e8dc21af182c5cf745ae70b6b2532749452655
Amaado <andresamadocibreiro22@gmail.com>
2024-02-02 21:30:02 +0100

    autoCommit 2024/02/02. Hora: 21:30

--- programacion/IntelliJ_/UD5/src/SalidaAUnArchivo.java
+++ programacion/IntelliJ_/UD5/src/SalidaAUnArchivo.java
@@ -2,16 +2,18 @@ import java.io.*;
 
 public class SalidaAUnArchivo {
     public static void main(String[] args) {
-        // Ruta del archivo en el que quieres escribir
-        String entrada = "archivo_salida.txt";
-        String salida = "salida.txt";
-        try{
-            FileReader fileReader = new FileReader(entrada);
-            FileWriter fileWriter = new FileWriter(salida);
-            int caracter;
-            while ((caracter=fileReader.read())!=-1){
 
+        String rutaArchivo = "archivo_salida.txt";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter printWriter = new PrintWriter(new FileWriter(rutaArchivo))) {
+            String linea;
+
+            while (!(linea = reader.readLine()).equalsIgnoreCase("salir")) {
+                printWriter.println(linea);
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
