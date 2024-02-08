21a1b89c8605c51c2528faf07ab982ff43c50dac
Amaado <andresamadocibreiro22@gmail.com>
2024-02-08 21:35:12 +0100

    autoCommit 2024/02/08. Hora: 21:35

--- autogit.bat
+++ autogit.bat
@@ -2,7 +2,7 @@ cd C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives
 @echo off
 start "" "C:\Users\DAM1_Alu02\Documents\GitHub\DAM1_archives\Dam1_web-main\pages\Cc2.png"
 timeout /t 1 >nul
-taskkill /f /im "visor de fotos de Windows.exe"
+taskkill /f /im Microsoft.Photos.exe
 
 git add --all
 git commit -m "autoCommit %date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2%"
