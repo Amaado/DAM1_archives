9134371bd605dd32cc4f5150d9070819e9bb733e
Amaado <andresamadocibreiro22@gmail.com>
2024-02-16 21:30:03 +0100

    autoCommit 2024/02/16. Hora: 21:30

---
+++ programacion/IntelliJ_/contornos/PRUEBAS Junit/src/CalculatorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+}
