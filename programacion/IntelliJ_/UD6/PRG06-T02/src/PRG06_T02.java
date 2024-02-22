b5d094c0c23e5379578b49b473f096648ccc4ceb
Amaado <andresamadocibreiro22@gmail.com>
2024-02-22 16:27:14 +0100

    .

--- programacion/IntelliJ_/UD6/PRG06-T02/src/PRG06_T02.java
+++ programacion/IntelliJ_/UD6/PRG06-T02/src/PRG06_T02.java
@@ -1,19 +1,22 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class PRG06_T02 {
     public static void main(String[] args) {
         double num = 0;
-        double[] ar1 = new double[20];
-        for (int i=0;i<=ar1.length;i++){
+        int[] ar1 = new int[20];
+        for (int i=0;i<ar1.length;i++){
             num = Math.random()*100;
             if (num == 100){
                 i--;
                 continue;
             }
+            int numInt = (int) num;
+            ar1[i]=numInt;
 
-            ar1[i]=num;
+
         }
-
+        System.out.println(Arrays.toString(ar1));
 
     }
 }
