1aea64307d08c4fa7fbb8cf36dc0da9e2ba5dcc3
Amaado <andresamadocibreiro22@gmail.com>
2024-02-28 21:47:01 +0100

    .

--- programacion/IntelliJ_/UD6/PRG07-T04/src/PRG07_T04.java
+++ programacion/IntelliJ_/UD6/PRG07-T04/src/PRG07_T04.java
@@ -5,9 +5,9 @@ public class PRG07_T04 {
         Scanner entrada = new Scanner(System.in);
 
         String [][] tablero = new String[8][8];
-        String negro = "░░";
-        String blanco = "▓▓";
-        String dama = "♛";
+        final String negro = "░░";
+        final String blanco = "▓▓";
+        final String dama = "♛";
         System.out.println("  ╭—————————————————————————╮");
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
@@ -53,68 +53,78 @@ public class PRG07_T04 {
 
         }
 
-        System.out.println("\nA que posicion quieres que mueva la reina?");
-        String posicion = entrada.nextLine();
-        posicion = posicion.toLowerCase();
 
-        String posicionNum = posicion.substring(1);
-        int numColumna = posicion.charAt(0)-97;
-        int numFila = 8 - Integer.parseInt(posicionNum);
 
-        System.out.println("  ╭—————————————————————————╮");
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
+        while (true){
+            System.out.println("\nA que posicion quieres que mueva la reina? ('.' para salir)");
+            String posicion = entrada.nextLine();
 
-                if (i==numFila && j==numColumna){
-                    tablero[i][j] = dama;
+            if (posicion.equals(".")){
+                break;
+            }
+
+
+            posicion = posicion.toLowerCase();
+            String posicionNum = posicion.substring(1);
+            int numColumna = posicion.charAt(0)-97;
+            int numFila = 8 - Integer.parseInt(posicionNum);
+
+            System.out.println("  ╭—————————————————————————╮");
+            for(int i=0;i<8;i++){
+                for(int j=0;j<8;j++){
+
+                    if (i==numFila && j==numColumna){
+                        tablero[i][j] = dama;
+                        if (j==0){
+                            System.out.print(8-i+" │ ");
+                        }
+                        System.out.print(tablero[i][j]+" ");
+                        continue;
+                    }
                     if (j==0){
                         System.out.print(8-i+" │ ");
                     }
-                    System.out.print(tablero[i][j]+" ");
-                    continue;
-                }
-                if (j==0){
-                    System.out.print(8-i+" │ ");
-                }
-                if (j==7 && i==0){
-                    tablero[i][j]=blanco;
-                    System.out.print(tablero[i][j]+" │");
-                }else {
-                    if(j==7){
-                        if ((i+j) % 2 ==0){
-                            tablero[i][j]=negro;
-                            System.out.print(tablero[i][j]+" │");
-                        }else {
-                            tablero[i][j]=blanco;
-                            System.out.print(tablero[i][j]+" │");
-                        }
+                    if (j==7 && i==0){
+                        tablero[i][j]=blanco;
+                        System.out.print(tablero[i][j]+" │");
                     }else {
-                        if ((i+j) % 2 ==0){
-                            tablero[i][j]=negro;
-                            System.out.print(tablero[i][j]+" ");
+                        if(j==7){
+                            if ((i+j) % 2 ==0){
+                                tablero[i][j]=negro;
+                                System.out.print(tablero[i][j]+" │");
+                            }else {
+                                tablero[i][j]=blanco;
+                                System.out.print(tablero[i][j]+" │");
+                            }
                         }else {
-                            tablero[i][j]=blanco;
-                            System.out.print(tablero[i][j]+" ");
+                            if ((i+j) % 2 ==0){
+                                tablero[i][j]=negro;
+                                System.out.print(tablero[i][j]+" ");
+                            }else {
+                                tablero[i][j]=blanco;
+                                System.out.print(tablero[i][j]+" ");
+                            }
                         }
                     }
+
+
+                }
+                System.out.print("\n");
+            }
+
+            System.out.println("  ╰—————————————————————————╯");
+
+            for (char letra='A';letra<='H';letra++){
+                if (letra == 'A'){
+                    System.out.print("    "+letra+"  ");
+                }else {
+                    System.out.print(letra+"  ");
                 }
 
-
             }
-            System.out.print("\n");
         }
 
-        System.out.println("  ╰—————————————————————————╯");
-
-        for (char letra='A';letra<='H';letra++){
-            if (letra == 'A'){
-                System.out.print("    "+letra+"  ");
-            }else {
-                System.out.print(letra+"  ");
-            }
-
-        }
-
+        System.out.println("Adiós!");
 
 
     }
