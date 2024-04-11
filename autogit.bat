c7558cc1ba2fa4bf71593b4ca0770c2a719e90d5
Amaado <andresamadocibreiro22@gmail.com>
2024-04-11 21:58:25 +0200

    2024/04/11. Hora: 21:58 | IP: 192.168.56.1

--- autogit.bat
+++ autogit.bat
@@ -7,7 +7,14 @@ for /f "tokens=2 delims=:" %%a in ('ipconfig ^| find "IPv4"') do (
 set ip_address=%ip_address:~1%
 
 # Agregar los comandos de Git
+cd \Dam1_web
 git add --all
 git commit -m "%date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2% | IP: %ip_address%"
 git push
+
+cd ..\
+git add --all
+git commit -m "%date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2% | IP: %ip_address%"
+git push
+
 exit
