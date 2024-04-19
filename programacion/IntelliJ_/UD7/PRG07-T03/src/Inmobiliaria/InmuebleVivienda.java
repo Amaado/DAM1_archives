03d4030e4d4de771f8358351f7630a80873d2527
Amaado <andresamadocibreiro22@gmail.com>
2024-04-19 17:47:05 +0200

    revert

--- programacion/IntelliJ_/UD7/PRG07-T03/src/Inmobiliaria/InmuebleVivienda.java
+++ programacion/IntelliJ_/UD7/PRG07-T03/src/Inmobiliaria/InmuebleVivienda.java
@@ -7,15 +7,15 @@ public abstract non-sealed class InmuebleVivienda extends Inmueble{
     private final int minHabitaciones = 3;
     private int numBanhos;
     private int numHabitaciones;
-    private ArrayList<Habitacion> habitaciones;
+    private ArrayList<Inmobiliaria.Habitacion> habitaciones;
 
     public InmuebleVivienda() {
         super();
-        habitaciones = new Habitacion[minHabitaciones];
+        habitaciones = new Inmobiliaria.Habitacion[minHabitaciones];
         numHabitaciones = 0;
     }
 
-    public InmuebleVivienda(int id, String direccion, double precio, double m2, int numBanhos, int numHabitaciones, Habitacion[] habitaciones) {
+    public InmuebleVivienda(int id, String direccion, double precio, double m2, int numBanhos, int numHabitaciones, Inmobiliaria.Habitacion[] habitaciones) {
         super(id, direccion, precio, m2);
         this.habitaciones = habitaciones.clone();
         this.numHabitaciones = habitaciones.length;
