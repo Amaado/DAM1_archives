9602a25971ed14f1302948e38eed8f800ae607a5
Amaado <146996634+Amaado@users.noreply.github.com>
2024-04-25 16:03:31 +0200

    .

---
+++ lmsxi/Trimestre 3/amado_cibreiro_andres_LM9_1_Ej1/script.js
@@ -0,0 +1,45 @@
+while (true){
+    variable = prompt ('Escribe alguna orden:');
+    if(variable=="salir"){
+        break;
+    }
+
+    if(isNaN(variable)){
+        alert (variable +' no es un numero');
+    }else{
+        alert (variable +' es un numero');
+    }
+}
+
+alert ('Adios!');
+
+function mostrarImagen(imageId, opacity, times) {
+    console.log(`ImageID: ${imageId}`);
+    console.log(`Opcity: ${opacity}`);
+    console.log(`Times: ${times}`);
+    var imagen = document.getElementById(imageId);
+    imagen.style.opacity = 0;
+
+    setTimeout(function () {
+      imagen.style.display = "block";
+      imagen.style.opacity = 0;
+    }, 0);
+
+    setTimeout(function () {
+      imagen.style.opacity = opacity[0];
+    }, times[0]);
+
+    setTimeout(function () {
+      imagen.style.opacity = opacity[1];
+    }, times[1]);
+
+    setTimeout(function () {
+      imagen.style.opacity = 0;
+    }, times[2]);
+
+    setTimeout(function () {
+      imagen.style.opacity = "0";
+      imagen.style.display = "none";
+    }, 8000);
+  }
+
