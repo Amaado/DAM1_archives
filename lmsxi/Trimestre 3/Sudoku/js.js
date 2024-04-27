244c8724913ac7a2f2d88164f068ecf81b624ba0
Amado <andresamadocibreiro22@gmail.com>
2024-04-27 22:31:53 +0200

    ,

--- lmsxi/Trimestre 3/Sudoku/js.js
+++ lmsxi/Trimestre 3/Sudoku/js.js
@@ -1,67 +1,61 @@
+for (let i = 1; i <= 9; i++) {
+  for (let j = 1; j <= 9; j++) {
+    document.getElementById("input" + i + "." + j).onblur =
+      function comprobar() {
+        let input = document.getElementById("input" + i + "." + j);
+        let celda = document.getElementById(i + "." + j);
 
-  
-  for(let i=1; i<=9; i++){
-      for(let j=1; j<=9; j++){
-      
-          document.getElementById('input'+i+'.'+j).onblur = function comprobar() {
-          let input = document.getElementById("input"+i+'.'+j);
-          let celda = document.getElementById(i+'.'+j);
-  
-          if(isNaN (input.value)){
-              celda.style.backgroundColor = "#b82200a9";
-  
-          }else if(0<input.value){
-              celda.style.backgroundColor = "";
-  
-          }else{
-              celda.style.backgroundColor = "#45ff34ad";
-          }
-      }
+        if (isNaN(input.value)) {
+          //input.style.backgroundColor = "#b82200a9";
+          input.style.background = "url('img/redSpot.png') center no-repeat";
+          input.style.backgroundSize = "cover" ;
+          
+          //celda.style.zIndex=1;
+
+        } else if (0 < input.value) {
+          input.style.background = "";
+
+        } else {
+          //input.style.backgroundColor = "#45ff34ad";
+          
+          input.style.background = "url('img/greenSpot.png') center no-repeat";
+          input.style.backgroundSize = "cover" ;
+          
+          //input.style.color = "#c8bda177";
+          //input.style.color = "red";
+          //input.style.mixBlendMode = "";
+          
+
+
+        }
+      };
   }
 }
 
-document.getElementById("botonComprobar").onclick = function comprobarBoton() {
-    for(let i=1; i<=9; i++){
-        for(let j=1; j<=9; j++){
-            
-            let input = document.getElementById("input"+i+'.'+j);
-            let celda = document.getElementById(i+'.'+j);
-    
-            if(isNaN (input.value)){
-                celda.style.backgroundColor = "#b82200a9";
-    
-            }else if(0<input.value){
-                celda.style.backgroundColor = "";
-    
-            }else{
-                celda.style.backgroundColor = "#45ff34ad";
-            }
-        }
-    }
-}
 
 document.getElementById("botonLimpiar").onclick = function limpiar() {
-    for(let i=1; i<=9; i++){
-      for(let j=1; j<=9; j++){
-          
-          let input = document.getElementById("input"+i+'.'+j);
-          let celda = document.getElementById(i+'.'+j);
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      let input = document.getElementById("input" + i + "." + j);
+      let celda = document.getElementById(i + "." + j);
 
-          celda.style.backgroundColor = "#ffffff00";
-          restaurarHover();
-          
-      }
+      input.style.background = "none";
+      restaurarHover();
+    }
   }
-  
-}
+};
+
 
 function restaurarHover() {
-  var cells = document.querySelectorAll('td');
+  let cells = document.querySelectorAll("td");
 
-  cells.forEach(function(cell) {
-      cell.style.backgroundColor = '';
+  cells.forEach(function (cell) {
+    
   });
 }
+
+
+
 /*
 function ForbidenInput(input) {
   var valor = input.value;
@@ -72,45 +66,57 @@ function ForbidenInput(input) {
   }
 }
 */
-document.addEventListener('DOMContentLoaded', function hovers() {
-  var cells = document.querySelectorAll('td');
+document.addEventListener("DOMContentLoaded", function hovers() {
+  var cells = document.querySelectorAll("td");
 
   function handleClick() {
-    cells.forEach(function(cell) {
-      cell.classList.remove('hover-row');
-      cell.classList.remove('hover-column');
+    cells.forEach(function (cell) {
+      cell.classList.remove("hover-row");
+      cell.classList.remove("hover-column");
     });
 
     var cellId = this.id;
-    var [row, column] = cellId.split('.');
+    var [row, column] = cellId.split(".");
 
-    cells.forEach(function(target) {
-      if (target.id.startsWith(row + '.')) {
-        target.classList.add('hover-row');
+    cells.forEach(function (target) {
+      if (target.id.startsWith(row + ".")) {
+        target.classList.add("hover-row");
       }
-      if(target.id.endsWith('.' + 3) || target.id.endsWith('.' + 6) || target.id.endsWith('.' + 9)){
-        target.classList.add('hover-border-replace');
+      if (
+        target.id.endsWith("." + 3) ||
+        target.id.endsWith("." + 6) ||
+        target.id.endsWith("." + 9)
+      ) {
+        target.classList.add("hover-border-replace");
       }
     });
 
-    cells.forEach(function(target) {
-      if (target.id.endsWith('.' + column)) {
-        target.classList.add('hover-column');
+    cells.forEach(function (target) {
+      if (target.id.endsWith("." + column)) {
+        target.classList.add("hover-column");
       }
     });
   }
 
-  cells.forEach(function(cell) {
-    cell.addEventListener('click', handleClick);
+  cells.forEach(function (cell) {
+    cell.addEventListener("click", handleClick);
   });
 
-  document.addEventListener('click', function(event) {
-    if (!event.target.closest('td')) {
-      cells.forEach(function(cell) {
-        cell.classList.remove('hover-row');
-        cell.classList.remove('hover-column');
+  document.addEventListener("click", function (event) {
+    if (!event.target.closest("td")) {
+      cells.forEach(function (cell) {
+        cell.classList.remove("hover-row");
+        cell.classList.remove("hover-column");
       });
     }
   });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const iconoAjustes = document.getElementById("settings");
+  const menu = document.getElementById("menu");
+
+  iconoAjustes.addEventListener("click", function() {
+    menu.classList.toggle("active");
+  });
+});
