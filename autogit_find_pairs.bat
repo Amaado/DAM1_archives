a7a728e2778e7bacf0a3077820e64713f424fe69
Amaado <andresamadocibreiro22@gmail.com>
2024-05-13 21:52:53 +0200

    ,

--- autogit_find_pairs.bat
+++ autogit_find_pairs.bat
@@ -7,7 +7,7 @@ for /f "tokens=2 delims=:" %%a in ('ipconfig ^| find "IPv4"') do (
 set ip_address=%ip_address:~1%
 
 # Agregar los comandos de Git
-cd ..\Dam1_web
+cd ..\FindPairs
 git add --all
 git commit -m "%date:~-4%/%date:~3,2%/%date:~0,2%. Hora: %time:~0,2%:%time:~3,2% | IP: %ip_address%"
 git push
